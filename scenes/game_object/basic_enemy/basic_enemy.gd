5844f6da0f7e0ce2609d26393b70744f84aabcd0
Oron Ben Zvi <oronbz@gmail.com>
2023-07-13 18:00:06 +0300

    Add enemy animations walk&death

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,13 +3,16 @@ extends CharacterBody2D
 const MAX_SPEED = 40
 
 @onready var health_component = $HealthComponent as HealthComponent
-
+@onready var visuals = $Visuals
 
 func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
-
+	
+	var move_sign = sign(velocity.x)
+	if velocity.x != 0:
+		visuals.scale.x = sign(-velocity.x)
 
 func get_direction_to_player():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
